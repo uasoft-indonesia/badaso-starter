23e14bf19c66996217096bc95c5a0dc4486111b0
afdal-develop <afdal-develop@gmail.com>
2022-03-08 21:37:33 +0700

    update docker compose file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\Badaso\BadasoSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(BadasoSeeder::class);
     }
 }
