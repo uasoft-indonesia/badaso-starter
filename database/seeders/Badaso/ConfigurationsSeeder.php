b6cbf61aa21ba3bd76e8e59d2749edfc7729054f
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-12-14 16:39:59 +0700

    update badaso starter use laravel 8

--- database/seeders/Badaso/ConfigurationsSeeder.php
+++ database/seeders/Badaso/ConfigurationsSeeder.php
@@ -130,6 +130,26 @@ class ConfigurationsSeeder extends Seeder
                     'group' => 'adminPanel',
                     'can_delete' => 0,
                 ],
+                11 => [
+                    'key' => 'defaultRoleRegistration',
+                    'display_name' => 'Default Role Registration',
+                    'value' => 'customer',
+                    'details' => '{"items":[]}',
+                    'type' => 'select',
+                    'order' => 1,
+                    'group' => 'adminPanel',
+                    'can_delete' => 0,
+                ],
+                12 => [
+                    'key' => 'timeWaitResendToken',
+                    'display_name' => 'Time wait to resend token',
+                    'value' => '60',
+                    'details' => '{"items":[]}',
+                    'type' => 'number',
+                    'order' => 1,
+                    'group' => 'adminPanel',
+                    'can_delete' => 0,
+                ],
             ];
 
             foreach ($settings as $key => $value) {
